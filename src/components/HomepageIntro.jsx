b650f69b82f0e344b6bf28baba8c76ea270d55bf
Reuvin Hernandez <reuvinhernandez22@gmail.com>
2023-12-11 19:48:01 +0800

    Update

---
+++ src/components/HomepageIntro.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function HomepageIntro() {
+  return (
+    <>
+        <div className='intro-container'>
+            <div className="intro">
+                <h2>Hi, I'm <span>Reuvin Hernandez</span></h2>
+                <h1>WEB DEVELOPER</h1>
+                <div className="buttons">
+                    <button>Hire Me</button>
+                    <button>Download CV</button>
+                </div>
+            </div>
+        </div>
+    </>
+  )
+}
+
+export default HomepageIntro
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <>
+        <div className="navBar">
+            <h2>My <span>Portfolio</span></h2>
+            <div className="navBar-container">
+                <a href="">Home</a>
+                <a href="">About</a>
+                <a href="">Skills</a>
+                <a href="">About</a>
+                <a href="">Message</a>
+            </div>
+        </div>
+    </>
+  )
+}
+
+export default Navbar
