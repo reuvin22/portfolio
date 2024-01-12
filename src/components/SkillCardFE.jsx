4bee547537301106453789f64ea0b7e41cce35e1
Reuvin Hernandez <reuvinhernandez22@gmail.com>
2024-01-12 22:08:04 +0800

    Update

--- src/components/SkillCardFE.jsx
+++ src/components/SkillCardFE.jsx
@@ -9,10 +9,8 @@ export default function SkillCardFE(props) {
             <img src={props.skill} alt="Skills" className='imgSkill'/>
             <img src={props.skill1} alt="Skills" className='imgSkill'/>
             <img src={props.skill2} alt="Skills" className='imgSkill'/>
-        </div>
-        <div className='skillset'>
-          <img src={props.skill3} alt="Skills" className='imgSkill'/>
-          <img src={props.skill4} alt="Skills" className='imgSkill'/>
+            <img src={props.skill3} alt="Skills" className='imgSkill'/>
+            <img src={props.skill4} alt="Skills" className='imgSkill'/>
         </div>
     </div>
     </>
