344253baa5bcd033578dc1cc00f057f56fda48dd
Reuvin Hernandez <reuvinhernandez22@gmail.com>
2024-05-24 21:51:11 +0800

    Update

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -10,12 +10,12 @@ function Sidebar() {
         setBurger(false);
       }
   return (
-    <div>
+    <div className='sm:fixed sm:flex sm:top-0 sm:w-full sm:z-50'>
         <div className="flex place-content-between relative xl:mx-20 xl:my-5 lg:mx-20 lg:my-5 md:mx-10 sm:mx-10 z-10">
           <img src="https://firebasestorage.googleapis.com/v0/b/projectimages-a2f47.appspot.com/o/Portfolio%2FLogo1.png?alt=media&token=f3e8428f-61f3-4cbc-9334-be41e1c498ea" alt="Name Logo" className='h-32 w-32 my-1 sm:h-28 sm:w-28' id='logo'/>
           <img src="https://firebasestorage.googleapis.com/v0/b/projectimages-a2f47.appspot.com/o/Portfolio%2Fburger.png?alt=media&token=2f1e79ab-c60f-4c8b-9813-00ee819fe218" 
             alt="Burger Button" 
-            className="md:hidden lg:hidden xl:hidden sm:w-14 sm:h-14 cursor-pointer my-7"
+            className="md:hidden lg:hidden xl:hidden sm:w-14 sm:h-14 cursor-pointer my-7 sm:ml-28"
             onClick={handleBurgerClick}
             id='logo'/>
           <div className="flex gap-10 text-2xl place-items-center mx-10 sm:hidden" id='nav'>
@@ -31,10 +31,10 @@ function Sidebar() {
               className='h-10 w-10 absolute right-0 mt-10 mr-10'
               onClick={handleCloseClick}/>
             <div className="grid place-items-center h-full mt-5 text-2xl">
-              <a href='#home' className='navLinks'>Home</a>
-              <a href='#abouts' className='navLinks'>About</a>
-              <a href='#proj' className='navLinks'>Projects</a>
-              <a href='#contacts' className='navLinks'>Contact</a>
+              <a href='#home' className='navLinks' onClick={handleCloseClick}>Home</a>
+              <a href='#abouts' className='navLinks' onClick={handleCloseClick}>About</a>
+              <a href='#proj' className='navLinks' onClick={handleCloseClick}>Projects</a>
+              <a href='#contacts' className='navLinks' onClick={handleCloseClick}>Contact</a>
             </div>
           </div>
     </div>
