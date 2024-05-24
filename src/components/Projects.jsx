57723fe9fa4385e10cf7634ee35e642e65581875
Reuvin Hernandez <reuvinhernandez22@gmail.com>
2024-05-25 01:40:36 +0800

    Update

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -111,7 +111,7 @@ function Projects() {
                   ))
                 )}
                 <div className='flex relative justify-center items-center gap-5 lg:hidden xl:hidden'>
-                  <img src="https://firebasestorage.googleapis.com/v0/b/projectimages-a2f47.appspot.com/o/Portfolio%2Farrow.png?alt=media&token=e3b654bd-ac5e-4eb8-863f-52a89bda040a" alt="Arrow" className={skillPage === 0 ? 'hidden' : `h-12 w-12 cursor-pointer rotate-180`} onClick={() => handlePrevPage()}/>
+                  <img src="https://firebasestorage.googleapis.com/v0/b/projectimages-a2f47.appspot.com/o/Portfolio%2Farrow.png?alt=media&token=e3b654bd-ac5e-4eb8-863f-52a89bda040a" alt="Arrow" className={skillPage === 0 ? 'hidden' : `h-12 w-12 cursor-pointer rotate-180 absolute ml-[-120%] `} onClick={() => handlePrevPage()}/>
                   {menu === 'fullstack' && (
                     <div className='max-w-72 h-72 border-2 border-white rounded-lg px-10 text-white relative overflow-hidden cursor-pointer hover:bg-fuchsia-600 sm:h-96 '
                     >
@@ -124,7 +124,7 @@ function Projects() {
                       </div>
                     </div>
                   )}
-                  <img src="https://firebasestorage.googleapis.com/v0/b/projectimages-a2f47.appspot.com/o/Portfolio%2Farrow.png?alt=media&token=e3b654bd-ac5e-4eb8-863f-52a89bda040a" alt="Arrow" className={skillPage === fullstack.length ? 'hidden' : `h-12 w-12 cursor-pointer`} onClick={() => handleNextPage()}/>
+                  <img src="https://firebasestorage.googleapis.com/v0/b/projectimages-a2f47.appspot.com/o/Portfolio%2Farrow.png?alt=media&token=e3b654bd-ac5e-4eb8-863f-52a89bda040a" alt="Arrow" className={skillPage === fullstack.length ? 'hidden' : `h-12 w-12 cursor-pointer absolute mr-[-120%]`} onClick={() => handleNextPage()}/>
                 </div>
               </div>
             </div>
